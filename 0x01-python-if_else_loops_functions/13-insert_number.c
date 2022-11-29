c7fe61a1a5d41d0911158ddd80b3bc869a3f872c
Mams9 <maameahma@yahoo.com>
2022-11-29 13:38:51 -0800

    If else function

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,32 +4,46 @@
 /**
  * insert_node - Inserts a number into a sorted singly-linked list.
  * @head: A pointer the head of the linked list.
- * @number: The number to insert.
+ * @number: The index of list where the new node isi added.
  *
  * Return: If the function fails - NULL.
  *         Otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+	listint_t *new;
+	listint_t *h;
+	listint_t *h_prev;
 
+	h = *head;
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
-	new->n = number;
 
-	if (node == NULL || node->n >= number)
+	while (h != NULL)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
 	}
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+	new->n = number;
 
-	new->next = node->next;
-	node->next = new;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
+	}
 
 	return (new);
 }
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in 'abdcefghijklmnopqrstuvwxyz':
+for i in 'abcdefghijklmnopqrstuvwxyz':
     print("{}".format(i), end="")
